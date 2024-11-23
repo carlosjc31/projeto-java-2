c08e2eb83e019dac688c96a14b3d94ee3974cb7c
carlosjc31 <carlosjc31@hotmail.com>
2024-11-23 19:34:37 -0300

    colocando inf. na agenda

---
+++ agenda-backend/src/main/java/com/agenda/agenda_backend/modelos/Agenda.java
@@ -0,0 +1,43 @@
+package com.agenda.agenda_backend.modelos;
+
+import java.util.Date;
+
+public class Agenda {
+    //Atributos
+    private int id;
+    private Date data;
+    private String descricao;
+    private String local;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public Date getData() {
+        return data;
+    }
+    public void setData(Date data) {
+        this.data = data;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public String getLocal() {
+        return local;
+    }
+    public void setLocal(String local) {
+        this.local = local;
+    }
+    
+    
+    //Métodos
+    
+
+
+    
+    
+}
--- agenda-backend/src/main/java/com/agenda/agenda_backend/resources/AgendaController.java
+++ agenda-backend/src/main/java/com/agenda/agenda_backend/resources/AgendaController.java
@@ -1,13 +1,32 @@
 package com.agenda.agenda_backend.resources;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.agenda.agenda_backend.modelos.Agenda;
 
 @RestController
 public class AgendaController {
  
     @GetMapping("agenda")
-    public String getAgenda(){
-        return "Bem-vindo a Agenda";
+    public Agenda getAgendas() throws ParseException {
+
+        Agenda ag = new Agenda();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        // o SimpleDateFormat serve para converter a String para Date
+
+        ag.setId(1);
+        Date data = sdf.parse("02/11/2024 08:00:00");
+        ag.setData(data);
+        ag.setDescricao("Prova de Glauco");
+        ag.setLocal("Fatec Itu, lab 6");
+
+
+        return ag;
     }
 }
